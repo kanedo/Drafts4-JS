0bb8960d08f6f88add38f8e27a248baff17222bd
Gabriel Bretschner <software@kanedo.net>
2014-10-23 10:52:54 +0200

    added a README and some docs

---
+++ README.md
@@ -0,0 +1,6 @@
+# Drafts4 JS Mock Object
+
+in version 4 of drafts for iOS you can develop actions with JS. To test your code
+on your computer you can use this simple draft-mock-object.
+
+For more information about javascript actions in drafts have a look [at the documentation](https://agiletortoise.zendesk.com/hc/en-us/articles/202771590-Action-Step-Script)
--- draft.js
+++ draft.js
@@ -13,22 +13,44 @@ draft.prototype.accessedDate = '';
 draft.prototype.flagged = false;
 draft.prototype.defineTag = function( tagName, tagString ){}
 
+/**
+ * usually persists the changes to the drafts object
+ * the mock function only logs the given object
+ **/
 function commit( draft ){
   console.log( draft );
 }
 
+/**
+ * returns an arbitrary string functioning as clipboard
+ * @return string
+ **/
 function getClipboard(){
   return _clipboard;
 }
-
+/**
+ * set the mock clipboard content
+ * note: it's not the real clipboard
+ * @param string message
+ **/
 function setClipboard( message ){
   _clipboard = message;
 }
 
+/**
+ * just a mock function so no error will occur
+ **/
 function stopAction(){
   return 0;
 }
 
-function markdown(string, useXHTML){
-  return string;
+/**
+ * should convert the string to markdown
+ * @todo find a lib to convert it to markdown
+ * @param string message
+ * @param bool useXHTML
+ * @return string
+ **/
+function markdown(message, useXHTML){
+  return message;
 }
